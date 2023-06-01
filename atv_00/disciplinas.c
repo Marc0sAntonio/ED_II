1e5504999890039a6b1b4b017ac6eecb614e61c0
Marc0sAntonio <marcos.brito@ufpi.edu.br>
2023-06-01 00:21:11 -0300

    att

--- atv_00/disciplinas.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct disciplina{
-    int _cod; // não pode ser repetido, é o parametro de organização da arv
-    char _nome_disciplina[40];
-    int _bloco; //deve ser menor que a quatidade de blocos do curso
-    int _carga_horaria; //multiplo do numerod e semanas da disciplina
-};
--- atv_00/main.c
+++ atv_00/main.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "curso.h"
+#include "arv_BB.h"
 
 int main() {
-    Curso *raizCurso;
-    raizCurso = NULL;
+    int cod = 0, bloco = 0;
+    Curso *raizCursos;
+    //Disciplina *raizDisciplinas = NULL;
+    raizCursos = NULL;
 
-    cria_arv_cursos(&raizCurso);
-    Imprime_cursos_crescente(raizCurso);
+    Cria_arv_cursos(&raizCursos);
+    Imprime_cursos_crescente(raizCursos);
+    
+    printf("Codigo para buscar o curso: ");
+    scanf("%d", &cod);
+    Imprime_por_cod(raizCursos, cod);
+    printf("Bloco para buscar o cursos: ");
+    scanf("%d", &bloco);
+    Imprime_cursos_bloco(raizCursos, bloco);
+    
+    printf("\n\nCodigo do curso que deseja inserir a disciplina: ");
+    scanf("%d", &cod);
+    Cria_arv_disc(&(raizCursos), cod);
+
+    //Cria_arv_disc(&(raizCursos), cod);
+
+    //imprimirDisciplinas(raizCursos); 
+    Imprimir_disc_em_ordem_curso(raizCursos, cod);
+
 
     return 0;
 }
